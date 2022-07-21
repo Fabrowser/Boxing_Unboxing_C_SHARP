bd1da0ec0ed48e29c90ec5de748e158bf10f23f7
Fabrowser <79614829+Fabrowser@users.noreply.github.com>
2022-07-20 22:45:39 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Boxing_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Boxing - Transforma tipo valor em tipo referencia
+            int x = 20;
+            Object y = x;
+            Console.WriteLine(y + " - Boxing para Tipo Referência");
+
+            //Boxing - Transforma tipo Referencia em tipo valor
+            int w = (int) y;
+            Console.WriteLine(w + " - Unboxing para Tipo Valor");
+
+
+
+
+        }
+    }
+}
